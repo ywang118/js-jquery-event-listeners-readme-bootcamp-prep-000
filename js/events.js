9a7d249c8f048793536ea0a9568aa43cfc79ccb4
wangfanqoo@126.com Wang <wangfanqoo@126.com>
2018-04-22 17:14:00 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -13,7 +13,9 @@ function frameIt(){
 
 function pressIt(){
   $("#typing").on("keydown",function(key){
-    if(key.which )
+    if(key.which == 71) {
+      alert('g was pressed');
+    }
   })
 }
 $(document).ready(function(){
