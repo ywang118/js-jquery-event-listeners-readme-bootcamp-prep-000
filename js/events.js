f47d23eaa7cbd1e7b2f0350139facaec2604595c
wangfanqoo@126.com Wang <wangfanqoo@126.com>
2018-04-22 17:39:52 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,29 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('hey!')
+  })
+}
+
+function frameIt(){
+  $("img").on('load',function(){
+    $(this).addClass("tasty");
+  })
+}
+
+function pressIt(){
+  $("#typing").on("keydown",function(key){
+    if(key.which == 71) {
+      alert('g was pressed');
+    }
+  })
+}
+
+function submitIt(){
+  $("form").on("submit",function(){
+    alert("Your form is going to be submitted now.")
+  })
+}
 
 $(document).ready(function(){
-
-// call functions here
-
 });
